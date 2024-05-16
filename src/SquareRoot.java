9376d367921745b8282d6d88b726c2004edb485a
Vitalii Kozachyna <v.kozachyna@gmail.com>
2024-05-16 23:18:54 +0300

    3rd commit

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -10,17 +10,35 @@ public class SquareRoot {
         int count = 0;
 
         while(true){
+            if (num > 0 ){
+                y_next = (num/y_prev + y_prev)/2;
+                epsilon = y_prev - y_next;
+                y_prev = y_next;
 
-            y_next = (num/y_prev + y_prev)/2;
-            epsilon = y_prev - y_next;
-            y_prev = y_next;
-
-            count++;
-            if ((count > 1) && (epsilon < 0.0001)) {
-                System.out.println("square root of " + num + " equals " + y_next);
-                System.out.println("count = " + count);
-                System.out.println("epsilon = " + epsilon);
+                count++;
+                if ((count > 1) && (epsilon < 0.0001)) {
+                    System.out.println("square root of " + num + " equals " + y_next);
+                    System.out.println("count = " + count);
+                    System.out.println("epsilon = " + epsilon);
+                    break;
+                }
+            } else if (num == 0) {
+                System.out.println("square root of " + num + " equals " + num);
                 break;
+            } else if (num < 0){
+
+//                y_next = ((-num)/y_prev + y_prev)/2;
+//                epsilon = y_prev - y_next;
+//                y_prev = y_next;
+//
+//                count++;
+//                if ((count > 1) && (epsilon < 0.0001)) {
+//                    System.out.println("square root of " + num + " equals " + y_next + "i");
+//                    System.out.println("count = " + count);
+//                    break;
+//                }
+
+                throw new IllegalArgumentException();
             }
         }
     }
