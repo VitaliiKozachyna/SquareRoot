bff8f1a9b4e67a1273bd45c8ca2ebc0e62782359
Vitalii Kozachyna <v.kozachyna@gmail.com>
2024-05-16 22:54:16 +0300

    Second commit

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class SquareRoot {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int num = scn.nextInt();
-        System.out.println("Square root of " + num + " equals " + Math.sqrt(num));
+        double num = scn.nextDouble();
+        double y_prev = 1;
+        double y_next;
+        double epsilon;
+        int count = 0;
+
+        while(true){
+
+            y_next = (num/y_prev + y_prev)/2;
+            epsilon = y_prev - y_next;
+            y_prev = y_next;
+
+            count++;
+            if ((count > 1) && (epsilon < 0.0001)) {
+                System.out.println("square root of " + num + " equals " + y_next);
+                System.out.println("count = " + count);
+                System.out.println("epsilon = " + epsilon);
+                break;
+            }
+        }
     }
 }
